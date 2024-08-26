8e2ba23df6245978a5034fd1d3aea98296908b2d
Jason Falk <jasonfalk9@gmail.com>
2024-08-26 11:43:37 -0700

    Add search bar to header.

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,18 +1,15 @@
 import React from "react";
-// import TextInput from 'react-native'
 
 function Header() {
-  const [number, onChangeNumber] = React.useState('');
   return (
     <React.Fragment>
       <button id="home-button">Home</button>
       <button id="notification-button">Notifications</button>
       <button id="message-button">Messages</button>
-      {/* <TextInput
-        onChangeText={onChangeNumber}
-        value={number}
-        placeholder="Search"
-      /> */}
+      <form>
+        <input type="string" id="search" name="search" placeholder="Search"/>
+      </form>
+      <button id="tweet-button">Tweet</button>
     </React.Fragment>
   );
 }
