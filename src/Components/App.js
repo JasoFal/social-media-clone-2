3f28cbe7c4a44e90b52cf59d7dcf5f944e91d5ad
Jason Falk <jasonfalk9@gmail.com>
2024-08-27 11:30:07 -0700

    Add post history to app.

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './Header';
 import NameCard from './NameCard';
+import PostHistory from './PostHistory';
 
 function App() {
   return (
     <React.Fragment>
       <Header />
       <NameCard />
+      <PostHistory />
     </React.Fragment>
   );
 }
