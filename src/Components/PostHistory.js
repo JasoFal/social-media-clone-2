fc8baa9d380843b7725676b5bc819edecc247094
Jason Falk <jasonfalk9@gmail.com>
2024-08-27 11:29:52 -0700

    Add a component to display all posts.

---
+++ src/Components/PostHistory.js
@@ -0,0 +1,36 @@
+import React from "react";
+import Post from "./Post";
+
+const mainPostList = [
+  {
+    name: "Lorem Ipsum 1",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+  },
+  {
+    name: "Lorem Ipsum 2",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+  },
+  {
+    name: "Lorem Ipsum 3",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+  },
+]
+
+function PostHistory() {
+  return (
+    <React.Fragment>
+      <form>
+        <input type="string" id="posthist" name="posthist" placeholder="What's happening?"/>
+      </form>
+      <hr/>
+      {mainPostList.map((post, index) =>
+        <Post name={post.name}
+          text={post.text}
+          key={index}/>
+      )}
+    </React.Fragment>
+  );
+}
+
+export default PostHistory;
+
